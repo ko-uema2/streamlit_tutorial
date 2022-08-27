b143e8d95497fb2f57008fbbcc872227f0740b5f
ko-uema2 <ko-uema2@outlook.jp>
2022-08-27 22:44:03 +0900

    initial commit

---
+++ apps/home.py
@@ -0,0 +1,13 @@
+import imp
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def app():
+    st.title('Home')
+    st.write("This is a sample home page in the multiapp.")
+    st.write("See 'app/home.py' to know how to use it.")
+    st.markdown("### Sample Data")
+    st.write("Navigate to 'Data Stats' page to visualize the data")
